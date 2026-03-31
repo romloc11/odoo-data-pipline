d6c8854d9154f5b782c6912d8b15fc2c6c37ed86
Román Alejandro López Ruvalcaba <romloc1706@hotmail.com>
2026-03-31 10:13:45 -0600

    Update extract.py

--- extract/extract.py
+++ extract/extract.py
@@ -1,1 +1,21 @@
 
+# autenticación
+common = xmlrpc.client.ServerProxy(f"{ODOO_URL}/xmlrpc/2/common")
+uid = common.authenticate(ODOO_DB, ODOO_USER, ODOO_API_KEY, {})
+
+models = xmlrpc.client.ServerProxy(f"{ODOO_URL}/xmlrpc/2/object")
+
+# extraer clientes
+clientes = models.execute_kw(
+    ODOO_DB,
+    uid,
+    ODOO_API_KEY,
+    'res.partner',
+    'search_read',
+    [[]],
+    {'fields': ['name','email','phone']}
+)
+
+df = pd.DataFrame(clientes)
+
+df.to_parquet("data/bronze/clientes.parquet")
