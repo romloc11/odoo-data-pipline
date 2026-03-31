17d15278cd103bc5d6cd039e5ba99f4b2cb15529
Román Alejandro López Ruvalcaba <romloc1706@hotmail.com>
2026-03-31 10:13:08 -0600

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,10 @@
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ODOO_URL = os.getenv("ODOO_URL")
+ODOO_DB = os.getenv("ODOO_DB")
+ODOO_USER = os.getenv("ODOO_USER")
+ODOO_API_KEY = os.getenv("ODOO_API_KEY")
